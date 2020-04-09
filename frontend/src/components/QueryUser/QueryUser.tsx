60c4876077911f29e4eaf309ba09010680e97940
Scott Pierce-Wrobel <scott.pierce@friends.dds.mil>
2020-04-09 18:43:25 -0400

    wip adding second table to admin page

---
+++ frontend/src/components/QueryUser/QueryUser.tsx
@@ -0,0 +1,90 @@
+import React, { useState, useCallback } from "react";
+import { Input, Select } from "solo-uswds";
+import {
+  WarehouseUser,
+  PaginatedApiResponse,
+  createFakeUsers
+} from "solo-types";
+import { useAuthContext } from "context";
+
+interface QueryUser {
+  options: Array<string>;
+  defaultOption?: string;
+}
+
+const QueryUser: React.FC<QueryUser> = ({ options }) => {
+  const { apiCall } = useAuthContext();
+
+  const [currentOption, setCurrentOption] = useState(options[0]);
+
+  const [currentValue, setCurrentValue] = useState("");
+
+  const onOptionChange: React.ChangeEventHandler<HTMLSelectElement> = event => {
+    setCurrentOption(event.currentTarget.value);
+  };
+
+  const onValueChange: React.ChangeEventHandler<HTMLInputElement> = event => {
+    setCurrentValue(event.currentTarget.value);
+    fetchWarehouseUser();
+  };
+
+  const [user, setUser] = useState<WarehouseUser[]>([]);
+
+  const fetchWarehouseUser = useCallback(async () => {
+    try {
+      const { results } = await apiCall<PaginatedApiResponse<WarehouseUser[]>>(
+        `/warehouse/user?q=${currentValue}`,
+        {
+          method: "GET"
+        }
+      );
+      setUser(results);
+      // suggestions(results)
+    } catch (e) {
+      setUser(createFakeUsers(1));
+    }
+  }, [setUser, apiCall]);
+
+  //   const suggestions = (results) => {
+  //     const options = results.map(r => (
+  //       <li key={r.id}>
+  //         {r.name}
+  //       </li>
+  //     ))
+  //     return <ul>{options}</ul>
+  //   }
+
+  return (
+    <form className="grid-row flex-row flex-justify-center flex-align-center">
+      <div className="grid-col-full tablet:grid-col-2 margin-x-2">
+        <Select
+          onChange={onOptionChange}
+          value={currentOption}
+          placeholder="Field"
+          defaultMargin
+          defaultOutline
+        >
+          {options.map(value => (
+            <option key={value} value={value}>
+              {value}
+            </option>
+          ))}
+        </Select>
+      </div>
+      <div className="grid-col-10 tablet:grid-col-3">
+        <Input
+          defaultMargin
+          value={currentValue}
+          onChange={onValueChange}
+          type="text"
+          placeholder="Add New User"
+        />
+      </div>
+      <div className="grid-col-full margin-x-2">
+        <p>{user}</p>
+      </div>
+    </form>
+  );
+};
+
+export default QueryUser;
--- frontend/src/pages/AdminPage/AdminPage.tsx
+++ frontend/src/pages/AdminPage/AdminPage.tsx
@@ -3,6 +3,7 @@ import { Title, Table } from "components";
 import createColumns from "./tableColumns";
 import useWarehouseUsers from "./useWarehouseUsers";
 import { WarehouseUser } from "solo-types";
+import QueryUser from "components/QueryUser/QueryUser";
 
 const AdminPage: React.FC = () => {
   const {
@@ -21,6 +22,12 @@ const AdminPage: React.FC = () => {
   return (
     <div className="tablet:margin-x-8 overflow-x-auto">
       <Title>User Administration</Title>
+      {/* show all users with the same aac as the current manager/user */}
+      <SelectFilterControls options={filterable} onSubmit={setGlobalFilter} />
+      <Table<WarehouseUser> columns={tableColumns} data={users} />
+      {/* <QueryUser /> */}
+      {/* show search results based on edipi (backend username) or last name (backend last_name) */}
+      <SelectFilterControls options={filterable} onSubmit={setGlobalFilter} />
       <Table<WarehouseUser> columns={tableColumns} data={users} />
     </div>
   );
